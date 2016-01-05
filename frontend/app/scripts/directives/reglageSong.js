db76466b439c0e03367fc6d9d528023a085f16da
SUN QUAN <sunquanmw@gmail.com>
2016-01-05 23:30:25 +0100

    Add play/pause button to the reglage

---
+++ frontend/app/scripts/directives/reglageSong.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('frontendApp')
+    .directive('myTest', function() {
+        function link(scope, element, attrs) {
+            var reglagePlay = angular.element(element.children()[0]);
+            var reglagePause = angular.element(element.children()[1]);
+
+            function setMasterVolume() {
+
+                var fraction = parseInt(masterVolumeSlider.value) / parseInt(masterVolumeSlider.max);
+                // Let's use an x*x curve (x-squared) since simple linear (x) does not
+                // sound as good.
+                if( masterVolumeNode != undefined)
+                    masterVolumeNode.gain.value = fraction * fraction;
+            }
+
+            function playFrom() {
+                setMasterVolume();
+            }
+            $(reglagePlay).on('click', function() {
+                //playFrom();
+                reglagePlay.css({'display' : 'none'});
+                reglagePause.css({'display' : 'inline-block'});
+            });
+            $(reglagePause).on('click', function() {
+                reglagePlay.css({'display' : 'inline-block'});
+                reglagePause.css({'display' : 'none'});
+            });
+
+        }
+        return {
+            restrict : 'E',
+            link : link
+        }
+    });
