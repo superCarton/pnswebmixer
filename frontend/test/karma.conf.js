af2ac01aa3b91ca6e2a19c8a4df157e05435897f
Vincent Montalieu <vincent.montalieu@outlook.com>
2016-01-14 08:52:04 +0100

    ajout de ngdraggable + debut de CSS pour loop et pattern

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/webcomponentsjs/webcomponents.js',
+      'bower_components/ngDraggable/ngDraggable.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
