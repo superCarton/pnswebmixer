00bfc8d420683dddb8d36802631a21e862566ec7
Vincent Montalieu <vincent.montalieu@outlook.com>
2016-01-18 16:52:44 +0100

    update front

--- frontend/app/scripts/controllers/patternMaker.js
+++ frontend/app/scripts/controllers/patternMaker.js
@@ -11,8 +11,9 @@ angular.module('frontendApp')
   .controller('PatternMakerCtrl', function ($scope) {
 
     $scope.droppedObjects1 = [];
+    var lightsIDs = ["#light-1", "#light-2", "#light-3", "#light-4", "#light-5", "#light-6", "#light-7", "#light-8"]
 
-    $scope.onDropComplete1 = function(data) {
+    $scope.onDropComplete1 = function (data) {
       var index = $scope.droppedObjects1.indexOf(data);
       if (index == -1) {
         $scope.droppedObjects1.push(data);
@@ -23,4 +24,22 @@ angular.module('frontendApp')
       angular.element(event.currentTarget).toggleClass("fa-circle-thin");
       angular.element(event.currentTarget).toggleClass("fa-circle");
     };
+
+    $scope.playBeat = function () {
+      $("#play").toggleClass("playing");
+      setTimeout(function () {
+        for (var i = 0; i < lightsIDs.length; i++) {
+          $(lightsIDs[i]).toggleClass("fa-circle-thin").toggleClass("fa-circle");
+          console.log(lightsIDs[i]);
+        }
+      }, 1000);
+    };
+
+    $scope.stopBeat = function () {
+      if ($("#play").hasClass("playing")) {
+        $("#play").toggleClass("playing");
+        $("#light-1").toggleClass("fa-circle-thin").toggleClass("fa-circle");
+      }
+    };
+
   });
