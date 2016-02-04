c379b02d225955badd3116aade70e5e2e188aa33
Vincent Montalieu <vincent.montalieu@outlook.com>
2016-02-04 14:51:42 +0100

    Ajout d'un spin cursor lors du login et sign up pour attendre + ajout de modals pour login sign up + cursor pointer pour le step sequencer

--- frontend/app/scripts/controllers/login.js
+++ frontend/app/scripts/controllers/login.js
@@ -13,6 +13,7 @@ angular.module('frontendApp')
     /************* INIT ****************/
 
     var json_to_send;
+    var dialog;
 
     $scope.init = function () {
       $scope.logoutMenuOn = false;
@@ -73,30 +74,102 @@ angular.module('frontendApp')
         $scope.lastName = data.last_name;
         $scope.email = data.email;
         $scope.pwd = data.password;
+
+        dialog = new BootstrapDialog({
+          size: BootstrapDialog.SIZE_SMALL,
+          title: "Inscription réussie",
+          message: "Bienvenue dans le Polytech Web Mixer, " + data.first_name.bold() + ".",
+        });
+        dialog.realize();
+        dialog.getModalHeader().css('background-color', '#5cb85c');
+        dialog.getModalHeader().css('color', '#ffffff');
+        dialog.getModalHeader().css('border-top-left-radius', '6px');
+        dialog.getModalHeader().css('border-top-right-radius', '6px');
+        dialog.open();
+
       }, function (err) {
-        //TODO: handle server error
+        if (err != null) {
+          dialog = new BootstrapDialog({
+            title: "Erreur",
+            message: "Il semble y avoir un problème avec le serveur... :-(",
+          });
+        } else {
+          dialog = new BootstrapDialog({
+            title: "Erreur",
+            message: err
+          });
+        }
+        dialog.realize();
+        //dialog.getModalHeader().css('background-color', '#f0b054');
+        dialog.getModalHeader().css('background-color', '#d9534f');
+        dialog.getModalHeader().css('color', '#ffffff');
+        dialog.getModalHeader().css('border-top-left-radius', '6px');
+        dialog.getModalHeader().css('border-top-right-radius', '6px');
+        dialog.open();
       });
     }
 
     /************ LOG IN *************/
 
     $scope.logIn = function (mail, pwd) {
-      json_to_send = {
-        email: mail,
-        password: pwd
-      };
 
-      UserFactory.loginToServer(json_to_send).then(function (data) {
-        $rootScope.user_id = data._id;
-        $scope.connected = true;
-        $scope.logoutMenuOn = true;
-        $scope.firstName = data.first_name;
-        $scope.lastName = data.last_name;
-        $scope.email = data.email;
-        $scope.pwd = data.password;
-      }, function (err) {
-        //TODO: handle server error
-      });
+      if (mail && pwd) {
+
+        $("body, body a, body input, body button, .step-sequencer-button").css("cursor", "progress");
+
+        json_to_send = {
+          email: mail,
+          password: pwd
+        };
+
+        UserFactory.loginToServer(json_to_send).then(function (data) {
+          $("body, body a, body input, body button").css("cursor", "auto");
+          $("body button").css("cursor", "pointer");
+          $(".step-sequencer-button").css("cursor", "pointer");
+          $rootScope.user_id = data._id;
+          $scope.connected = true;
+          $scope.logoutMenuOn = true;
+          $scope.firstName = data.first_name;
+          $scope.lastName = data.last_name;
+          $scope.email = data.email;
+          $scope.pwd = data.password;
+
+          dialog = new BootstrapDialog({
+            size: BootstrapDialog.SIZE_SMALL,
+            title: "Connexion réussie",
+            message: "C'est un plaisir de vous revoir, " + data.first_name.bold() + ".",
+          });
+          dialog.realize();
+          dialog.getModalHeader().css('background-color', '#5cb85c');
+          dialog.getModalHeader().css('color', '#ffffff');
+          dialog.getModalHeader().css('border-top-left-radius', '6px');
+          dialog.getModalHeader().css('border-top-right-radius', '6px');
+          dialog.open();
+
+        }, function (err) {
+          $("body, body a, body input").css("cursor", "auto");
+          $("body button").css("cursor", "pointer");
+          $(".step-sequencer-button").css("cursor", "pointer");
+          if (err != null) {
+            dialog = new BootstrapDialog({
+              title: "Erreur",
+              message: "Il semble y avoir un problème avec le serveur... :-(",
+            });
+          } else {
+            dialog = new BootstrapDialog({
+              title: "Erreur",
+              message: err
+            });
+          }
+          dialog.realize();
+          //dialog.getModalHeader().css('background-color', '#f0b054');
+          dialog.getModalHeader().css('background-color', '#d9534f');
+          dialog.getModalHeader().css('color', '#ffffff');
+          dialog.getModalHeader().css('border-top-left-radius', '6px');
+          dialog.getModalHeader().css('border-top-right-radius', '6px');
+          dialog.open();
+        });
+      }
     }
 
   });
