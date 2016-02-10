0836020ac3bdc307f9fe5b7b1401e8e693aba5ea
Romain Guillot <rguillot1003@gmail.com>
2016-02-10 14:49:40 +0100

    Main

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,8 +8,9 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function (audioPlayer) {
 
+    var context = audioPlayer.initAudio();
 
 
   });
